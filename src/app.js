6ba968a15655bc92aec8521adacb10b165eb8784
your_crowley <your_crowley@your-crowleys-MacBook-Pro.local>
2025-01-04 19:35:27 +0200

    finished lab3

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ const express = require("express");
 const hbs = require("hbs");
 const axios = require("axios");
 const path = require('path');
+const {host, port} = require("./configuration");
+const {connectDB} = require("./helpers/dbHelper");
+const User = require("./models/User");
 
 
 const fs = require("fs");
 
-let data = fs.readFileSync("data/cities.json", "utf-8");
+let data = fs.readFileSync((path.join(__dirname, '/data/cities.json')), "utf-8");
 let cities = JSON.parse(data);
 
 
@@ -49,9 +52,62 @@ app.get('/weather/:city?', async (req, res) => {
 });
 
 
+
+app.get('/visitors-list', async (req, res) => {
+    try {
+        await connectDB();
+
+        const users = await User.find();
+        console.log(users);
+
+        res.status(200).json({ users });
+    } catch (error) {
+        console.error('Помилка при отриманні відвідувачів:', error);
+        res.status(500).json({ error: 'Помилка при отриманні відвідувачів' });
+    }
+});
+
+
+app.get('/visitors/:visitorName?', async (req, res) => {
+    const visitorName = req.params.visitorName || req.query.visitorName;
+  
+    try {
+      await connectDB();
+  
+      const newUser = new User({
+        name: visitorName
+      });
+  
+      await newUser.save();
+  
+      console.log('Відвідувач доданий:', newUser);
+      res.status(201).json({ message: 'Відвідувач доданий', user: newUser });
+    } catch (error) {
+      console.error('Помилка при додаванні відвідувача:', error);
+      res.status(500).json({ error: 'Помилка при додаванні відвідувача' });
+    }
+  });
+
+
 app.get('/login', (req, res) => {
     res.send("Hello, It's login page");
 });
 
-app.listen(3000, () => {
-});
+
+
+const startServer = () => {
+    app.listen(port, () => {
+        console.log(`API service is working on port ${port}`);
+        console.log(`Host is ${host}`)
+    });
+}
+
+
+(async () => {
+    try {
+      await connectDB();
+      startServer();
+    } catch (error) {
+      console.error("DB connection failed:", error);
+    }
+})();
---
+++ src/helpers/dbHelper.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const {db} = require("./../configuration");
+
+
+module.exports.connectDB = () => {
+    mongoose.connect(db, {useNewUrlParser: true})
+    return mongoose.connection
+}
