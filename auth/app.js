ac2bf1f9c7542c7101b321caf003a3e989312bb4
your_crowley <your_crowley@your-crowleys-MacBook-Pro.local>
2025-01-12 15:10:28 +0200

    finished lab 6

--- auth/app.js
+++ auth/app.js
@@ -7,6 +7,10 @@ let app = express();
 
 app.set('view engine', 'hbs');
 
+app.get('/health-check', (req, res) => {
+    res.status(200).json({"status": "ok"})
+})
+
 
 app.listen(port, () => {
     console.log(`Auth service is working on port ${port}`);
